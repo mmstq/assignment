080d1c8ec447d632bcd7fe74782511770de3e9ba
mmstq <mushtakkhan9@gmail.com>
2024-01-08 22:45:04 +0530

    logger removed

--- lib/data/http_service.dart
+++ lib/data/http_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:assignment/model/article_model.dart';
 import 'package:http/http.dart' as http;
-import 'package:logger/logger.dart';
 
 class HttpService {
   final String baseUrl = 'https://newsapi.org/v2/';
--- lib/provider/homepage_provider.dart
+++ lib/provider/homepage_provider.dart
@@ -1,7 +1,6 @@
 import 'package:assignment/data/http_service.dart';
 import 'package:assignment/model/article_model.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:logger/logger.dart';
 
 class HomeProvider extends ChangeNotifier {
   List<Article> articles = [];
